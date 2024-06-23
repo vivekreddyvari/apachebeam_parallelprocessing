979beeae82dc77ae35c8b891ee3992dc2371491c
vivekreddyvari <47451566+vivekreddyvari@users.noreply.github.com>
2024-06-24 00:07:08 +0200

    second-commit understood I want to go slowly

---
+++ main/co_group_by_key_with_dict.py
@@ -0,0 +1,56 @@
+#  Copyright 2022 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# standard libraries
+import logging
+
+# third party libraries
+import apache_beam as beam
+from apache_beam import CoGroupByKey, Create, Map
+
+
+def run(argv=None):
+    # Define the input key-value pairs.
+    jobs = [
+        ("Anna", "SWE"),
+        ("Kim", "Data Engineer"),
+        ("Kim", "Data Scientist"),
+        ("Robert", "Artist"),
+        ("Sophia", "CEO"),
+    ]
+    hobbies = [
+        ("Anna", "Painting"),
+        ("Kim", "Football"),
+        ("Kim", "Gardening"),
+        ("Robert", "Swimming"),
+        ("Sophia", "Mathematics"),
+        ("Sophia", "Tennis"),
+    ]
+
+    # Create two PCollections with beam.Create().
+    # Use beam.CoGroupByKey() to group elements from these two PCollections
+    # by their key.
+    with beam.Pipeline() as p:
+        jobs_pcol = p | "Create Jobs" >> Create(jobs)
+        hobbies_pcol = p | "Create Hobbies" >> Create(hobbies)
+        output = (
+            (jobs_pcol, hobbies_pcol)
+            | "CoGroupByKey" >> CoGroupByKey()
+            | "Log" >> Map(logging.info)
+        )
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    run()
